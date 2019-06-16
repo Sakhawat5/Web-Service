0f6fe090f78462a24be0f9fb721c201074f1d5af
Sakhaouat hosen <sakhawatcmt@gmail.com>
2019-06-16 22:19:50 +0600

    Summation solve

---
+++ PlantWebService/PlantWebService/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlantWebService.Models;
+
+namespace PlantWebService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            int a=20, b=30, sum;
+            sum = a + b;
+
+
+            return "This is my result = "+sum;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
