72c0d1d0e9fc9d550c2430989bce63be6635e1f1
Showkat Akbar <showkat131080@gmail.com>
2019-06-17 00:06:43 +0600

    add a class and mehod-

---
+++ PlantWebService/PlantWebService/Controllers/mmClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantWebService.Controllers
+{
+    public class mmClass
+    {
+        public int M()
+        {
+            return 10;
+        }
+    }
+}
